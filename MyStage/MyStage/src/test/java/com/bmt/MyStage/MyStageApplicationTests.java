0c89662d12e4826762e3ecad8e35d881dce39491
Lorenzo <lori.dallocco@gmail.com>
2025-06-03 10:41:42 +0200

    Add files via upload

---
+++ MyStage/MyStage/src/test/java/com/bmt/MyStage/MyStageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmt.MyStage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyStageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
