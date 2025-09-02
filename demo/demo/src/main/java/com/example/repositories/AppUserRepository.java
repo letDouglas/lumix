40c9d1d70be25431f0c14d6b39a53351cf913b28
Lorenzo <lori.dallocco@gmail.com>
2025-09-02 15:15:24 +0200

    Add files via upload

---
+++ demo/demo/src/main/java/com/example/repositories/AppUserRepository.java
@@ -0,0 +1,8 @@
+package com.example.repositories;
+
+import com.example.models.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppUserRepository extends JpaRepository<AppUser, Integer> {
+    public AppUser findByEmail(String email);
+}
